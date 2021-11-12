21b2438800c3dd173d28f984f829280dba721770
c-noelle <94136282+c-noelle@users.noreply.github.com>
2021-11-12 13:16:08 -0500

    Add files via upload

--- Model1a.java
+++ Model1a.java
@@ -19,6 +19,8 @@ public class Model1a {
 		System.out.println("climate zone: ");
 		double climate = userImput.nextDouble();
 		
+		userImput.close();
+		
 		//initializing variables
 		double hoursOfCharge = 0;
 
@@ -53,173 +55,199 @@ public class Model1a {
 				double PTimeToCharge = 13.5 / (solarPanelPower*hoursOfCharge) * 2;
 		
 		//figuring out equation for the Deka Solar
-		double CDSkwhPerCycle = 1.18;
+		double CDSkwhPerCycle = 0.049*24;
 		double CDScyclePerLife = 1000;
 		double CDScostReplacement = 3375;
 		double CDSbaseCost = 433;
 		double CDStotalCycles = continuouskWh / CDSkwhPerCycle;
 		double CDSnumReplaced = CDStotalCycles / CDScyclePerLife;
 		double CDSreplacementCost = CDScostReplacement*CDSnumReplaced;
-		double CDSbatteryOneUnRound = Math.round((CDSbaseCost+CDSreplacementCost)*100);
-		double CDSbatteryOne = CDSbatteryOneUnRound/100;
+		double CDSbatteryCostUnRound = Math.round((CDSbaseCost+CDSreplacementCost)*100);
+		double CDSbatteryCost = CDSbatteryCostUnRound/100;
+		double CDSbatteryTotal = continuouskWh/CDSkwhPerCycle;
+		double CDSbatteryTotalCostUnRound=Math.round(CDSbatteryTotal*CDSbatteryCost*100);
+		double CDSbatteryTotalCost=CDSbatteryTotalCostUnRound/100;
+		double CDSbatteryNumUnRound =Math.round(CDSbatteryTotalCost/CDSbatteryCost);
+		double CDSbatteryNum =CDSbatteryNumUnRound; 
+		double CDSbatteryTotalCostFinal=CDSbatteryNum * CDSbatteryCost;
 		
 		//figuring out the equation for the Trojan
-		double CTkwhPerCycle = 2.5;
+		double CTkwhPerCycle = 0.19*24;
 		double CTcyclePerLife = 500;
 		double CTcostReplacement = 3300;
 		double CTbaseCost = 1042;
 		double CTtotalCycles = continuouskWh / CTkwhPerCycle;
 		double CTnumReplaced = CTtotalCycles / CTcyclePerLife;
 		double CTreplacementCost = CTcostReplacement*CTnumReplaced;
-		double CTbatteryOneUnRound = Math.round((CTbaseCost+CTreplacementCost)*100);
-		double CTbatteryOne = CTbatteryOneUnRound/100;
+		double CTbatteryCostUnRound = Math.round((CTbaseCost+CTreplacementCost)*100);
+		double CTbatteryCost = CTbatteryCostUnRound/100;
+		double CTbatteryTotal = continuouskWh/CDSkwhPerCycle;
+		double CTbatteryTotalCostUnRound=Math.round(CTbatteryTotal*CTbatteryCost*100);
+		double CTbatteryTotalCost=CTbatteryTotalCostUnRound/100;
+		double CTbatteryNumUnRound =Math.round(CTbatteryTotalCost/CTbatteryCost);
+		double CTbatteryNum =CTbatteryNumUnRound; 
+		double CTbatteryTotalCostFinal=CTbatteryNum * CTbatteryCost;
 		
 		//figuring out the equation for the Discover
-		double CDkwhPerCycle = 7.4;
+		double CDkwhPerCycle = 6.65*24;
 		double CDcyclePerLife = 7100;
 		double CDcostReplacement = 6503;
 		double CDbaseCost = 6503;
 		double CDtotalCycles = continuouskWh / CDkwhPerCycle;
 		double CDnumReplaced = CDtotalCycles / CDcyclePerLife;
 		double CDreplacementCost = CDcostReplacement*CDnumReplaced;
-		double CDbatteryOneUnRound = Math.round((CDbaseCost+CDreplacementCost)*100);
-		double CDbatteryOne = CDbatteryOneUnRound/100;
+		double CDbatteryCostUnRound = Math.round((CDbaseCost+CDreplacementCost)*100);
+		double CDbatteryCost = CDbatteryCostUnRound/100;
+		double CDbatteryTotal = continuouskWh/CDkwhPerCycle;
+		double CDbatteryTotalCostUnRound=Math.round(CDbatteryTotal*CDbatteryCost*100);
+		double CDbatteryTotalCost=CDbatteryTotalCostUnRound/100;
+		double CDbatteryNumUnRound =Math.round(CDbatteryTotalCost/CDbatteryCost);
+		double CDbatteryNum =CDbatteryNumUnRound; 
+		double CDbatteryTotalCostFinal=CDbatteryNum * CDbatteryCost;
 		
 		//figuring out the equation for the Electriq
-		double CEkwhPerCycle = 10;
+		double CEkwhPerCycle = 7.6*24;
 		double CEcyclePerLife = 71000;
 		double CEcostReplacement = 13025;
 		double CEbaseCost = 13025;
 		double CEtotalCycles = continuouskWh / CEkwhPerCycle;
 		double CEnumReplaced = CEtotalCycles / CEcyclePerLife;
 		double CEreplacementCost = CEcostReplacement*CEnumReplaced;
-		double CEbatteryOneUnRound = Math.round((CEbaseCost+CEreplacementCost)*100);
-		double CEbatteryOne = CEbatteryOneUnRound/100;
+		double CEbatteryCostUnRound = Math.round((CEbaseCost+CEreplacementCost)*100);
+		double CEbatteryCost = CEbatteryCostUnRound/100;
+		double CEbatteryTotal = continuouskWh/CEkwhPerCycle;
+		double CEbatteryTotalCostUnRound=Math.round(CEbatteryTotal*CEbatteryCost*100);
+		double CEbatteryTotalCost=CEbatteryTotalCostUnRound/100;
+		double CEbatteryNumUnRound =Math.round(CEbatteryTotalCost/CEbatteryCost);
+		double CEbatteryNum =CEbatteryNumUnRound; 
+		double CEbatteryTotalCostFinal=CEbatteryNum * CEbatteryCost;
 		
 		//figuring out the equation for the Powerwall+ *check cycles per life*
-		double CPkwhPerCycle = 13.5;
+		double CPkwhPerCycle = 7*24;
 		double CPcyclePerLife = 10000000;
 		double CPcostReplacement = 13500;
 		double CPbaseCost = 13500;
 		double CPtotalCycles = continuouskWh / CPkwhPerCycle;
 		double CPnumReplaced = CPtotalCycles / CPcyclePerLife;
 		double CPreplacementCost = CPcostReplacement*CPnumReplaced;
-		double CPbatteryOneUnRound = Math.round((CPbaseCost+CPreplacementCost)*100);
-		double CPbatteryOne = CPbatteryOneUnRound/100;
+		double CPbatteryCostUnRound = Math.round((CPbaseCost+CPreplacementCost)*100);
+		double CPbatteryCost = CPbatteryCostUnRound/100;
+		double CPbatteryTotal = continuouskWh/CPkwhPerCycle;
+		double CPbatteryTotalCostUnRound=Math.round(CPbatteryTotal*CPbatteryCost*100);
+		double CPbatteryTotalCost=CPbatteryTotalCostUnRound/100;
+		double CPbatteryNumUnRound =Math.round(CPbatteryTotalCost/CPbatteryCost);
+		double CPbatteryNum =CPbatteryNumUnRound;
+		double CPbatteryTotalCostFinal=CPbatteryNum * CPbatteryCost;
 		
-		//if able to charge		
+		//if able to charge
 		if (DSTimeToCharge <= hoursOfCharge || TTimeToCharge <= hoursOfCharge || DTimeToCharge <= hoursOfCharge || ETimeToCharge <= hoursOfCharge || PTimeToCharge <= hoursOfCharge) {
 			
-		if (CDSbatteryOne<CTbatteryOne && CDSbatteryOne<CDbatteryOne && CDSbatteryOne<CEbatteryOne && CDSbatteryOne<CPbatteryOne) {
+		if (CDSbatteryTotalCost<CTbatteryTotalCost && CDSbatteryTotalCost<CDbatteryTotalCost && CDSbatteryTotalCost<CEbatteryTotalCost && CDSbatteryTotalCost<CPbatteryTotalCost) {
 			System.out.println("the best continous battery for you is the: Deka Solar 8GCC2 6V 198");
+			System.out.println("You would have to buy: " + CDSbatteryNum + " batteries.");
 		}
-		else if (CTbatteryOne<CDSbatteryOne && CTbatteryOne<CDbatteryOne && CTbatteryOne<CEbatteryOne && CTbatteryOne<CPbatteryOne) {
-		System.out.println("the best continous battery for you is the: Trojan L-16 -SPRE 6V 415");
+		else if (CTbatteryTotalCost<CDSbatteryTotalCost && CTbatteryTotalCost<CDbatteryTotalCost && CTbatteryTotalCost<CEbatteryTotalCost && CTbatteryTotalCost<CPbatteryTotalCost) {
+			System.out.println("the best continous battery for you is the: Trojan L-16 -SPRE 6V 415");
+			System.out.println("You would have to buy: " + CTbatteryNum + " batteries.");
 		}
-		else if (CDbatteryOne<CDSbatteryOne && CDbatteryOne<CTbatteryOne && CDbatteryOne<CEbatteryOne && CDbatteryOne<CPbatteryOne) {
-		System.out.println("the best continous battery for you is the: Discover AES 7.4 kWh");
+		else if (CDbatteryTotalCost<CDSbatteryTotalCost && CDbatteryTotalCost<CTbatteryTotalCost && CDbatteryTotalCost<CEbatteryTotalCost && CDbatteryTotalCost<CPbatteryTotalCost) {
+			System.out.println("the best continous battery for you is the: Discover AES 7.4 kWh");
+			System.out.println("You would have to buy: " + CDbatteryNum + " batteries.");
 		}
-		else if (CEbatteryOne<CDSbatteryOne && CEbatteryOne<CTbatteryOne && CEbatteryOne<CDbatteryOne && CEbatteryOne<CPbatteryOne) {
+		else if (CEbatteryTotalCost<CDSbatteryTotalCost && CEbatteryTotalCost<CTbatteryTotalCost && CEbatteryTotalCost<CDbatteryTotalCost && CEbatteryTotalCost<CPbatteryTotalCost) {
 			System.out.println("the best continous battery for you is the: Electriq PowerPod 2");
+			System.out.println("You would have to buy: " + CEbatteryNum + " batteries.");
 		}
-		else if (CPbatteryOne<CDSbatteryOne && CPbatteryOne<CTbatteryOne && CPbatteryOne<CDbatteryOne && CPbatteryOne<CEbatteryOne) {
+		else if (CPbatteryTotalCost<CDSbatteryTotalCost && CPbatteryTotalCost<CTbatteryTotalCost && CPbatteryTotalCost<CDbatteryTotalCost && CPbatteryTotalCost<CEbatteryTotalCost) {
 			System.out.println("the best continous battery for you is the: Tesla Powerwall+");
+			System.out.println("You would have to buy: " + CPbatteryNum + " batteries.");
 		}
-		double[] findMin = {CDSbatteryOne, CTbatteryOne, CDbatteryOne, CEbatteryOne, CPbatteryOne};
+		double[] findMin = {CDSbatteryTotalCostFinal, CTbatteryTotalCostFinal, CDbatteryTotalCostFinal, CEbatteryTotalCostFinal, CPbatteryTotalCostFinal};
 			Arrays.sort(findMin);
-			System.out.println("your expected price will be: $" + findMin[0]);
+			System.out.println("your expected price will be: $" + findMin[0] +
+					" ");
 	}
 
-		// initializing variables
-		double IhoursOfCharge = 0;
-
-		// hours of sun for each climate zone
-		if (climate == 1) {
-			hoursOfCharge = 2927.0;
-		} else if (climate == 2) {
-			hoursOfCharge = 2626.0;
-		} else if (climate == 3) {
-			hoursOfCharge = 2928.9;
-		} else if (climate == 4) {
-			hoursOfCharge = 2682.2;
-		} else if (climate == 5) {
-			hoursOfCharge = 3524.3;
-		} else if (climate == 6) {
-			hoursOfCharge = 2677.2;
-		} else if (climate == 7) {
-			hoursOfCharge = 2722.7;
-		}
-
-
-		// figuring out equation for the Deka Solar
-		double IDSkwhPerCycle = 1.18;
-		double IDScyclePerLife = 1000;
-		double IDScostReplacement = 3375;
-		double IDSbaseCost = 433;
-		double IDStotalCycles = instantaneouskWh / IDSkwhPerCycle;
-		double IDSnumReplaced = IDStotalCycles / IDScyclePerLife;
-		double IDSreplacementCost = IDScostReplacement * IDSnumReplaced;
-		double IDSbatteryOneUnRound = Math.round((IDSbaseCost + IDSreplacementCost) * 100);
-		double IDSbatteryOne = IDSbatteryOneUnRound / 100;
-
-		// figuring out the equation for the Trojan
-		double ITkwhPerCycle = 2.5;
-		double ITcyclePerLife = 500;
-		double ITcostReplacement = 3300;
-		double ITbaseCost = 1042;
-		double ITtotalCycles = instantaneouskWh / ITkwhPerCycle;
-		double ITnumReplaced = ITtotalCycles / ITcyclePerLife;
-		double ITreplacementCost = ITcostReplacement * ITnumReplaced;
-		double ITbatteryOneUnRound = Math.round((ITbaseCost + ITreplacementCost) * 100);
-		double ITbatteryOne = ITbatteryOneUnRound / 100;
+	
 
 		// figuring out the equation for the Discover
-		double IDkwhPerCycle = 7.4;
+		double IDkwhPerCycle = 14.4*24;
 		double IDcyclePerLife = 7100;
 		double IDcostReplacement = 0;
 		double IDbaseCost = 6503;
 		double IDtotalCycles = instantaneouskWh / IDkwhPerCycle;
 		double IDnumReplaced = IDtotalCycles / IDcyclePerLife;
 		double IDreplacementCost = IDcostReplacement * IDnumReplaced;
-		double IDbatteryOneUnRound = Math.round((IDbaseCost + IDreplacementCost) * 100);
-		double IDbatteryOne = IDbatteryOneUnRound / 100;
+		double IDbatteryCostUnRound = Math.round((IDbaseCost+IDreplacementCost)*100);
+		double IDbatteryCost = IDbatteryCostUnRound/100;
+		double IDbatteryTotal = continuouskWh/IDkwhPerCycle;
+		double IDbatteryTotalCostUnRound=Math.round(IDbatteryTotal*IDbatteryCost*100);
+		double IDbatteryTotalCost=IDbatteryTotalCostUnRound/100;
+		double IDbatteryNumUnRound =Math.round(IDbatteryTotalCost/IDbatteryCost);
+		double IDbatteryNum =IDbatteryNumUnRound;
+		double IDbatteryTotalCostFinal=IDbatteryNum * IDbatteryCost;
 
 		// figuring out the equation for the Electriq
-		double IEkwhPerCycle = 10;
+		double IEkwhPerCycle = 9*24;
 		double IEcyclePerLife = 71000;
 		double IEcostReplacement = 0;
 		double IEbaseCost = 13025;
 		double IEtotalCycles = instantaneouskWh / IEkwhPerCycle;
 		double IEnumReplaced = IEtotalCycles / IEcyclePerLife;
 		double IEreplacementCost = IEcostReplacement * IEnumReplaced;
-		double IEbatteryOneUnRound = Math.round((IEbaseCost + IEreplacementCost) * 100);
-		double IEbatteryOne = IEbatteryOneUnRound / 100;
+		double IEbatteryCostUnRound = Math.round((IEbaseCost+IEreplacementCost)*100);
+		double IEbatteryCost = IEbatteryCostUnRound/100;
+		double IEbatteryTotal = continuouskWh/IEkwhPerCycle;
+		double IEbatteryTotalCostUnRound=Math.round(IEbatteryTotal*IEbatteryCost*100);
+		double IEbatteryTotalCost=IEbatteryTotalCostUnRound/100;
+		double IEbatteryNumUnRound =Math.round(IEbatteryTotalCost/IEbatteryCost);
+		double IEbatteryNum =IEbatteryNumUnRound;
+		double IEbatteryTotalCostFinal=IEbatteryNum * IEbatteryCost;
 
 		// figuring out the equation for the Powerwall+ *check cycles per life*
-		double IPkwhPerCycle = 13.5;
+		double IPkwhPerCycle = 10*24;
 		double IPcyclePerLife = 10000000;
 		double IPcostReplacement = 0;
 		double IPbaseCost = 13500;
 		double IPtotalCycles = instantaneouskWh / IPkwhPerCycle;
 		double IPnumReplaced = IPtotalCycles / IPcyclePerLife;
 		double IPreplacementCost = IPcostReplacement * IPnumReplaced;
-		double IPbatteryOneUnRound = Math.round((IPbaseCost + IPreplacementCost) * 100);
-		double IPbatteryOne = IPbatteryOneUnRound / 100;
-
+		double IPbatteryCostUnRound = Math.round((IPbaseCost+IPreplacementCost)*100);
+		double IPbatteryCost = IPbatteryCostUnRound/100;
+		double IPbatteryTotal = continuouskWh/IPkwhPerCycle;
+		double IPbatteryTotalCostUnRound=Math.round(IPbatteryTotal*IPbatteryCost*100);
+		double IPbatteryTotalCost=IPbatteryTotalCostUnRound/100;
+		double IPbatteryNumUnRound =Math.round(IPbatteryTotalCost/IPbatteryCost);
+		double IPbatteryNum =IPbatteryNumUnRound;
+		double IPbatteryTotalCostFinal=IPbatteryNum * IPbatteryCost;
+		
 		// if able to charge
 		if (DTimeToCharge <= hoursOfCharge || ETimeToCharge <= hoursOfCharge || PTimeToCharge <= hoursOfCharge) {
 			
-			 if (IDbatteryOne < IEbatteryOne && IDbatteryOne < IPbatteryOne) {
+			 if (IDbatteryTotalCost < IEbatteryTotalCost && IDbatteryTotalCost < IPbatteryTotalCost) {
 				System.out.println("the best instantaneous battery for you is the: Discover AES 7.4 kWh");
-			} else if (IEbatteryOne < IDbatteryOne && IEbatteryOne < IPbatteryOne) {
+				System.out.println("You would have to buy: " + IDbatteryNum + " batteries.");
+				
+			} else if (IEbatteryTotalCost < IDbatteryTotalCost && IEbatteryTotalCost < IPbatteryTotalCost) {
 				System.out.println("the best instantaneous battery for you is the: Electriq PowerPod 2");
-			} else if (IPbatteryOne < IDbatteryOne && IPbatteryOne < IEbatteryOne) {
+				System.out.println("You would have to buy: " + IEbatteryNum + " batteries.");
+				
+			} else if (IPbatteryTotalCost < IDbatteryTotalCost && IPbatteryTotalCost < IEbatteryTotalCost) {
 				System.out.println("the best instantaneous battery for you is the: Tesla Powerwall+");
+				System.out.println("You would have to buy: " + IPbatteryNum + " batteries.");
+				
 			}
 			 
-			 double[] findMin = {IDbatteryOne, IEbatteryOne, IPbatteryOne };
+			 double[] findMin = {IDbatteryTotalCostFinal,IEbatteryTotalCostFinal, IPbatteryTotalCostFinal};
 			Arrays.sort(findMin);
-			System.out.println("your expected price will be: $" + findMin[0]);
+			
+			System.out.println("your expected price will be: $" + findMin[0] +
+					" ");
 	}
+		//System.out.println(CDSbatteryTotalCost + "," + CTbatteryTotalCost + "," + CDbatteryTotalCost + "," + CEbatteryTotalCost + "," + CPbatteryTotalCost);
+		//System.out.println(IDbatteryTotalCost + "," + IEbatteryTotalCost + "," + IPbatteryTotalCost);
+	
 	}
+	
+	
 }
